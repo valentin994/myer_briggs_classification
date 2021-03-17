cb392410d88b573791101e0deb34f57dd11995bd
Valentin Vareskic <valentin.vareskic@outlook.com>
2021-03-17 18:30:59 +0100

    Initial commit

---
+++ Plots.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+class Plots:
+    @staticmethod
+    def plot_distribution(df):
+        pass
+
+
+df = pd.read_csv("./data/mbti_1.csv")
+print(df.info())
